c978a3361206280d1e2be6ca767547ee93f4daab
lovixe <0@lovihe.cn>
2021-12-01 20:55:36 +0800

    处理一下杂项

--- test.py
+++ test.py
@@ -1,8 +1,32 @@
-import numpy as np
+import unittest
 
-def rmse(predictions):
-    return np.sqrt(((predictions) ** 2).mean())
+from packet import nodePacket
+from packet import packet
+from configure import config
+
+class TestConfigure(unittest.TestCase):
+    def test_weight(self):
+        a = config.weights
+        self.assertEqual(len(a), 4)
+
+#测试基本数据包
+class TestNodePacket(unittest.TestCase):
+    def test_init(self):
+        a = nodePacket(1)
+        result = True
+        for i in range(10000):
+            result = a.timeLapse()
+            if result == True:
+                self.assertNotEquals(i, 0)
+                break
+        self.assertTrue(result)
+
+#测试聚合的数据包
+class TestPacket(unittest.TestCase):
+    def test_remain(self):
+        a = packet()
+        self.assertGreater(a.getRemainSpace(), 1)
 
 
-a = [[1,1],[1,1],[1,1]]
-print(rmse(np.array(a)))
+if __name__ == '__main__':
+    unittest.main()
